63c3b4bf785f3d2d7d63e9da0b06c261fd268c35
Robert Kolner <robert.kolner@gmail.com>
2015-01-14 19:46:14 +0100

    Added django suit.

--- dusken_site/settings.py
+++ dusken_site/settings.py
@@ -28,6 +28,7 @@ LOCAL_APPS = (
     'dusken_api',
 )
 INSTALLED_APPS = (
+    'suit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -47,6 +48,12 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
+
+TEMPLATE_CONTEXT_PROCESSORS = TCP + (
+    'django.core.context_processors.request',
 )
 
 ROOT_URLCONF = 'dusken_site.urls'
@@ -141,3 +148,4 @@ try:
     from local_settings import *
 except ImportError:
     pass
+
