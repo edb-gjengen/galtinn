8147796fbbc4c6f6522d4192974273b33385d017
Nikolai R Kristiansen <nikolaik@gmail.com>
2016-03-23 17:45:33 +0100

    Add password reset via builtin forms

--- duskensite/settings.py
+++ duskensite/settings.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from django.core.urlresolvers import reverse_lazy
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
@@ -86,9 +88,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 AUTH_USER_MODEL = 'dusken.DuskenUser'
-LOGIN_REDIRECT_URL = '/me/'
-LOGIN_URL = '/login/'
-LOGOUT_URL = '/logout/'
+LOGIN_REDIRECT_URL = reverse_lazy('home')
+LOGIN_URL = reverse_lazy('login')
+LOGOUT_URL = reverse_lazy('logout')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
--- duskensite/urls.py
+++ duskensite/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.contrib.auth.views import login, logout, password_reset, password_reset_done
 from rest_framework.authtoken.views import obtain_auth_token
 
 from dusken import urls as dusken_urls
@@ -11,13 +10,7 @@ admin.autodiscover()
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include(dusken_urls)),
-    url(r'^auth/', obtain_auth_token)
-]
-
-# Auth
-urlpatterns += [
-    url(r'^login/$', login, name='login'),
-    url(r'^logout/$', logout, name='logout'),
-    url(r'^password_reset/$', password_reset, name='password_reset'),
-    url(r'^password_reset_done/$', password_reset_done, name='password_reset_done'),
+    url(r'^auth/', obtain_auth_token),
+    # Built in aut views
+    url('^user/', include('django.contrib.auth.urls'))
 ]
