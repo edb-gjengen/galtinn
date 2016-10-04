25ebb301ae734c0923efb65d1146fe9e34ed7aed
Nikolai Kristiansen <nikolai@zeppelin.no>
2016-10-04 10:37:48 +0200

    fix(api): Remove user auth tokens for now

--- dusken/apps.py
+++ dusken/apps.py
@@ -1,8 +1,10 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class DuskenAppConfig(AppConfig):
     name = 'dusken'
+    verbose_name = _('Dusken')
 
     def ready(self):
         import dusken.signals
