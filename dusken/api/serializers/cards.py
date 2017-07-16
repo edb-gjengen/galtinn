88d030ed0dd09322f7d410a76608577608c3aa6e
Jonas Braathen <jonas@ponas.no>
2017-07-16 21:23:54 +0200

    API: Refer to orders connected to member card

--- dusken/api/serializers/cards.py
+++ dusken/api/serializers/cards.py
@@ -6,4 +6,4 @@ from dusken.models import MemberCard
 class MemberCardSerializer(serializers.ModelSerializer):
     class Meta:
         model = MemberCard
-        fields = ('card_number', 'registered', 'is_active', 'user', 'created')
+        fields = ('card_number', 'registered', 'is_active', 'user', 'created', 'orders')
