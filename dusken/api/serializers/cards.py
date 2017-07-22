8681303a2f1d16603895bba8e141f911cca18d67
Jonas Braathen <jonas@ponas.no>
2017-07-22 19:48:15 +0200

    API: Refer to orders connected to cards by uuid

--- dusken/api/serializers/cards.py
+++ dusken/api/serializers/cards.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 
-from dusken.models import MemberCard
+from dusken.models import MemberCard, Order
 
 
 class MemberCardSerializer(serializers.ModelSerializer):
+    orders = serializers.SlugRelatedField(
+        read_only=True,
+        many=True,
+        slug_field='uuid')
+
     class Meta:
         model = MemberCard
         fields = ('card_number', 'registered', 'is_active', 'user', 'created', 'orders')
