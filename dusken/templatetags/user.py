3d17849300f1adf1531786bc87cc6475250fa35b
Nikolai R Kristiansen <nikolaik@gmail.com>
2017-08-08 02:20:21 +0200

    OrgUnitList and Detail: minor fixes

--- dusken/templatetags/user.py
+++ dusken/templatetags/user.py
@@ -5,4 +5,10 @@ register = template.Library()
 
 @register.filter(name='has_group')
 def has_group(user, group):
+    if user.is_superuser:
+        return True
+
+    if group is None:
+        return False
+
     return user.has_group(group)
