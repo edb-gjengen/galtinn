da73c75ed666ea695f021dfa060b4d30fdb78c44
Jonas Braathen <jonas@ponas.no>
2017-08-08 16:25:56 +0200

    Move Order.get_unclaimed_orders to an OrderManager

--- dusken/managers.py
+++ dusken/managers.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import UserManager
-from django.db.models import Q, QuerySet
+from django.db.models import Manager, Q, QuerySet
 from django.utils import timezone
 
 
@@ -18,6 +18,7 @@ class DuskenUserQuerySet(QuerySet):
     def by_membership_end_date(self):
         return self.order_by('-memberships__end_date')
 
+
 class DuskenUserManager(UserManager):
     def get_queryset(self):
         return DuskenUserQuerySet(self.model, using=self._db)
@@ -30,3 +31,19 @@ class DuskenUserManager(UserManager):
 
     def by_membership_end_date(self):
         return self.get_queryset().by_membership_end_date()
+
+
+class OrderQuerySet(QuerySet):
+    def unclaimed(self, phone_number=None, member_card=None):
+        return self.filter(
+            Q(user__isnull=True) & (
+                Q(phone_number__isnull=False, phone_number=phone_number) |
+                Q(member_card__isnull=False, member_card=member_card))).order_by('-created')
+
+
+class OrderManager(Manager):
+    def get_queryset(self):
+        return OrderQuerySet(self.model, using=self._db)
+
+    def unclaimed(self, phone_number=None, member_card=None):
+        return self.get_queryset().unclaimed(phone_number, member_card)
