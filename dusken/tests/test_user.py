ba5dd4493f8ad51c15f9d6ec9ffd1206a06b977f
Toralf Neby berge <toralf.berge@gmail.com>
2017-08-09 17:38:13 +0200

    Fixed test

--- dusken/tests/test_user.py
+++ dusken/tests/test_user.py
@@ -109,6 +109,7 @@ class DuskenUserActivateTestCase(TestCase):
             'first_name': 'Ola',
             'last_name': 'Nordmann',
             'email': 'olanord@example.com',
+            'password': 'password',
             'g-recaptcha-response': 'PASSED',
             'code': self.order.transaction_id[:8],
         }
