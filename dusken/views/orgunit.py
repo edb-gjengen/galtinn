9de45dff2629a485f470416bc02ad55621d4f92d
Toralf Neby Berge <toralf.berge@gmail.com>
2017-11-01 20:40:50 +0100

    Add admin first sorting in orgunit/edit/users

--- dusken/views/orgunit.py
+++ dusken/views/orgunit.py
@@ -1,9 +1,28 @@
+from itertools import chain
+
 from django.urls import reverse
 from django.views.generic import ListView, DetailView, UpdateView
 
 from dusken.models import OrgUnit, DuskenUser
 from dusken.views.mixins import VolunteerRequiredMixin
 from dusken.forms import UserWidgetForm, OrgUnitEditForm
+
+
+def get_sorted_users(view, context):
+    order_fields = ['first_name', 'last_name', 'username']
+    if context['orgunit'].admin_group:
+        admins = view.get_object().admin_group.user_set.order_by(*order_fields)
+    else:
+        admins = []
+
+    if context['orgunit'].group:
+        users = view.get_object().group.user_set.order_by(*order_fields).exclude(pk__in=admins)
+    else:
+        users = []
+
+    context['users_sorted'] = chain(admins, users)
+
+    return context
 
 
 class OrgUnitListView(VolunteerRequiredMixin, ListView):
@@ -19,21 +38,7 @@ class OrgUnitDetailView(VolunteerRequiredMixin, DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        order_fields = ['first_name', 'last_name', 'username']
-
-        if context['orgunit'].admin_group:
-            admins = self.get_object().admin_group.user_set.order_by(*order_fields)
-        else:
-            admins = []
-
-        if context['orgunit'].group:
-            users = self.get_object().group.user_set.order_by(*order_fields).exclude(pk__in=admins)
-        else:
-            users = []
-
-        context['users_sorted'] = admins | users
-
-        return context
+        return get_sorted_users(self, context)
 
 
 class OrgUnitEditView(VolunteerRequiredMixin, UpdateView):
@@ -55,17 +60,4 @@ class OrgUnitEditUsersView(VolunteerRequiredMixin, DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['user_search'] = UserWidgetForm
-        order_fields = ['first_name', 'last_name', 'username']
-        if context['orgunit'].admin_group:
-            admins = self.get_object().admin_group.user_set.order_by(*order_fields)
-        else:
-            admins = []
-
-        if context['orgunit'].group:
-            users = self.get_object().group.user_set.order_by(*order_fields).exclude(pk__in=admins)
-        else:
-            users = []
-
-        context['users_sorted'] = admins | users
-
-        return context
+        return get_sorted_users(self, context)
