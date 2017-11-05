8edc44472c79f43cf44aff810ba5a3baafb5adf6
Nikolai R Kristiansen <nikolaik@gmail.com>
2017-11-05 23:42:03 +0100

    Bump some deps

--- duskensite/settings/base.py
+++ duskensite/settings/base.py
@@ -158,7 +158,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissions',
     ],
-    'PAGE_SIZE': 25
+    'PAGE_SIZE': 25,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination'
 }
 
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
