0aa0c36c6125f727ad0378315a7643aeeee94598
Nikolai R Kristiansen <nikolaik@gmail.com>
2019-12-27 18:39:39 +0100

    🔥 Remove raven wsgi

--- duskensite/wsgi.py
+++ duskensite/wsgi.py
@@ -1,8 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from raven.contrib.django.middleware.wsgi import Sentry
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "duskensite.settings.prod")
 
 application = get_wsgi_application()
-application = Sentry(application)
