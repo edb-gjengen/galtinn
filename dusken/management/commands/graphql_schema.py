bdb2c01974a12c7f76207681eb07d8994155588e
Nikolai R Kristiansen <nikolaik@gmail.com>
2022-02-20 15:14:18 +0100

    ♻ Replace graphene with strawberry-graphql
    ✨ Add management command to generate schema

---
+++ dusken/management/commands/graphql_schema.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+from django.conf import settings
+from django.core.management import BaseCommand, CommandParser
+from strawberry.printer import print_schema
+
+from dusken.api.graphql import schema as schema_symbol
+
+
+class Command(BaseCommand):
+    help = " Write graphql schema to file or stdout"
+
+    def add_arguments(self, parser: CommandParser):
+        parser.add_argument("--filename", default="schema.graphql", help="`-` prints to stdout")
+
+    def handle(self, *args, filename=None, **options):
+        schema_output = print_schema(schema_symbol)
+        if filename != "-":
+            with Path(settings.BASE_DIR, filename).open("w+") as fp:
+                fp.write(schema_output)
+            self.stdout.write(self.style.SUCCESS("OK"))
+        else:
+            print(schema_output)
