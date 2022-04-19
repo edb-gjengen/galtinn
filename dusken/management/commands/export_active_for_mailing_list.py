c42c834af83a630172c621a966e97a1b791b25b2
Jonas Braathen <jonas@ponas.no>
2022-04-19 13:33:55 +0200

    ✨ Add management command for exporting active members as CSV

---
+++ dusken/management/commands/export_active_for_mailing_list.py
@@ -0,0 +1,40 @@
+import csv
+import sys
+import json
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from dusken.models import DuskenUser
+
+
+class Command(BaseCommand):
+    """
+    The CSV file should have the following headers with the exact column names.
+    attributes (optional) should be a valid JSON string with double escaped quotes.
+
+    Example:
+
+    email,name,attributes
+    user1@mail.com,"User One","{""age"": 42, ""planet"": ""Mars""}"
+    user2@mail.com,"User Two","{""age"": 24, ""job"": ""Time Traveller""}"
+    """
+
+    help = "Export users with an active membership and volunteer status as CSV"
+
+    def handle(self, *args, **options):
+        exportable = []
+        members = DuskenUser.objects.with_valid_membership()
+        for member in members:
+            if (member.is_volunteer and member.is_lifelong_member) or not member.is_lifelong_member:
+                exportable.append(
+                    {
+                        "email": member.email,
+                        "name": "{} {}".format(member.first_name, member.last_name),
+                        "attributes": json.dumps({"user_id": member.id}),
+                    }
+                )
+        writer = csv.DictWriter(sys.stdout, fieldnames=["email", "name", "attributes"])
+        writer.writeheader()
+        for row in exportable:
+            writer.writerow(row)
