c4fb69d45fb4d594292286870a6bec03fbddb9e5
Nikolai R Kristiansen <nikolaik@gmail.com>
2022-07-23 15:16:42 +0200

    🔧 Bump ZIP_TO_CITY_MAP

--- dusken/management/commands/zip_to_city.py
+++ dusken/management/commands/zip_to_city.py
@@ -7,12 +7,12 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    """Ref: http://www.bring.no/radgivning/sende-noe/adressetjenester/postnummer"""
+    """Ref: https://www.bring.no/radgivning/sende-noe/adressetjenester/postnummer"""
 
     help = "Update zip code to city map"
 
     def handle(self, *args, **options):
-        url = "http://www.bring.no/radgivning/sende-noe/adressetjenester/postnummer/_attachment/615728?_download=true&_ts=1595e62daf0"
+        url = "https://www.bring.no/radgivning/sende-noe/adressetjenester/postnummer/_attachment/615728?_download=true&_ts=1595e62daf0"
         lines = []
 
         r = requests.get(url)
@@ -22,7 +22,7 @@ class Command(BaseCommand):
             if not row:
                 continue
 
-            lines.append(f"    '{row[0]}': '{row[1]}',\n")  # f.ex '0559': 'OSLO',
+            lines.append(f'    "{row[0]}": "{row[1]}",\n')  # f.ex '0559': 'OSLO',
 
         with open(os.path.join(settings.BASE_DIR, "dusken/zip_to_city.py"), "w+") as out:
             out.write("ZIP_TO_CITY_MAP = {\n")
