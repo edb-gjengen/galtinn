093b474f844e6a23f2b238b9a6d5225e6829e73f
Leander Furumo <leanderfurumo@gmail.com>
2024-04-19 20:32:21 +0200

    add discord role id filter to groups api

--- dusken/api/views/groups.py
+++ dusken/api/views/groups.py
@@ -1,14 +1,18 @@
 from django.contrib.auth.models import Group as DjangoGroup
-from django_filters.rest_framework import DjangoFilterBackend, FilterSet
+from django_filters.rest_framework import BooleanFilter, DjangoFilterBackend, FilterSet
 from rest_framework import filters, mixins, viewsets
 
 from dusken.api.serializers.groups import GroupSerializer
 
 
 class GroupFilter(FilterSet):
+    no_discord_roles = BooleanFilter(
+        field_name="profile__discord_roles", lookup_expr="isnull"
+    )
+
     class Meta:
         model = DjangoGroup
-        fields = ("id", "name", "profile")
+        fields = ("id", "name", "profile", "profile__discord_roles__discord_id")
 
 
 class GroupViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
@@ -21,7 +25,7 @@ class GroupViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
         filters.SearchFilter,
     )
     filterset_class = GroupFilter
-    search_fields = ("id", "name", "profile")
+    search_fields = ("id", "name", "profile", "profile__discord_roles__discord_id")
     lookup_field = "id"
 
     def get_queryset(self):
