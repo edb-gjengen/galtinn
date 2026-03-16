682379c0e24bb86eb453cf7f8b1091941d2701db
Jonas Braathen <jonas@ponas.no>
2026-03-16 02:01:50 +0100

    Satisfy linter

--- dusken/tests/test_autocomplete.py
+++ dusken/tests/test_autocomplete.py
@@ -6,13 +6,17 @@ from django.urls import reverse
 from dusken.models import DuskenUser
 
 
+@pytest.mark.django_db
 @pytest.fixture
-def admin(db):
-    return DuskenUser.objects.create_superuser("admin", email="admin@example.com", password="password")
+def admin():
+    return DuskenUser.objects.create_superuser(
+        "admin", email="admin@example.com", password="password"
+    )
 
 
+@pytest.mark.django_db
 @pytest.fixture
-def foo_bar_user(db):
+def foo_bar_user():
     return DuskenUser.objects.create_user(
         "foobar", email="foobar@example.com", first_name="Foo", last_name="Bar"
     )
@@ -25,7 +29,7 @@ def search_autocomplete(client, query):
     return json.loads(response.content)
 
 
-def test_search_by_full_name(client, admin, foo_bar_user):
+def test_search_by_full_name(client, admin, foo_bar_user):  # noqa: ARG001
     """Searching 'Foo Bar' should find a user with first_name='Foo' and last_name='Bar'."""
     client.force_login(admin)
     data = search_autocomplete(client, "Foo Bar")
